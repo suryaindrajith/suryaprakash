6bc131916b42e609316d1ac97936f7e6aebe1879
surya <suryaprakashindrajith2006>
2026-04-19 15:15:26 +0530

    delete button added

--- MyExpenses/MyExpenses/ExpensesPage.xaml.cs
+++ MyExpenses/MyExpenses/ExpensesPage.xaml.cs
@@ -18,6 +18,17 @@ public partial class ExpensesPage : ContentPage
         base.OnAppearing();
         LoadData();
     }
+    private void OnDeleteClicked(object sender, EventArgs e)
+    {
+        var button = sender as Button;
+        var expense = button?.BindingContext as Expense;
+
+        if (expense != null)
+        {
+            AppData.Expenses.Remove(expense);
+            LoadData();
+        }
+    }
     private async void OnAddExpenseClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new AddExpensePage());
